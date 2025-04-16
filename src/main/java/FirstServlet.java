6050188e03aab6457a8ec966837f3a682176c142
Arjit Gautam <arjitgautamw2015@gmail.com>
2025-04-17 00:57:29 +0530

    generic

--- src/main/java/FirstServlet.java
+++ src/main/java/FirstServlet.java
@@ -11,6 +11,6 @@ public class FirstServlet extends GenericServlet {
     public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
         res.setContentType("text/html");
         PrintWriter out = res.getWriter();
-        out.println("<h1>Hello buddy, this is my first servlet</h1>");
+        out.println("<h1>Hello buddy, this is my first generic servlet</h1>");
     }
 }
